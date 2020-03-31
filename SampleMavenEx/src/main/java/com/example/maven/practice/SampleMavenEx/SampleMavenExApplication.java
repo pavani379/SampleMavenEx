6131297d77dbe37ccafbeea90be9038a05b2415d
=chinni <=pavanivatrapu379@gmail.com>
2020-03-31 17:07:12 +0530

    commit spring project

---
+++ SampleMavenEx/src/main/java/com/example/maven/practice/SampleMavenEx/SampleMavenExApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven.practice.SampleMavenEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMavenExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMavenExApplication.class, args);
+	}
+
+}
---
+++ SampleMavenEx/src/test/java/com/example/maven/practice/SampleMavenEx/SampleMavenExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.practice.SampleMavenEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMavenExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
